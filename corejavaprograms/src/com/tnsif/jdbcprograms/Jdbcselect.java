f2332cd3a3e5f7c8f69e1600183ec3fd677f2b64
krish <krish@tony>
2024-07-23 15:51:28 +0530

    commit message

--- corejavaprograms/src/com/tnsif/jdbcprograms/Jdbcselect.java
+++ corejavaprograms/src/com/tnsif/jdbcprograms/Jdbcselect.java
@@ -1,5 +1,16 @@
 package com.tnsif.jdbcprograms;
-
+import java.sql.*;
+import javax.sql.*;
 public class Jdbcselect {
-
+	public static void main(String[] args) {
+		
+	try {
+Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/sriindu","root","maadusindhu@123");
+Statement st=conn.createStatement();
+String strselect="Select booktitle,price,qty,from book";
+ResultSet rst=st.executeQuery(stselect);
+System.out.println("records are");
+int price=
+}
+}
 }
